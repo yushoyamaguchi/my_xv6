70b8eebb5e88192aa64a419036e6814f2f711678
yushoyamaguchi <ysh.824@outlook.jp>
2022-12-17 02:41:33 +0900

    tx/rx init 1

--- e1000.c
+++ e1000.c
@@ -5,10 +5,20 @@
 
 struct e1000 {
     uint32_t mmio_base;
-    struct rx_desc rx_ring[RX_RING_SIZE] __attribute__((aligned(16)));;
-    struct tx_desc tx_ring[TX_RING_SIZE] __attribute__((aligned(16)));;
+    struct rx_desc rx_ring[RX_RING_SIZE] __attribute__((aligned(16)));//V2Pした領域である必要がある？
+    struct tx_desc tx_ring[TX_RING_SIZE] __attribute__((aligned(16)));//V2Pした領域である必要がある？
     uint8_t irq;
 };
+
+unsigned int e1000_reg_read(struct e1000 *dev, uint16_t reg)
+{
+    return *(volatile uint32_t *)(dev->mmio_base + reg);
+}
+
+void e1000_reg_write(struct e1000 *dev, uint16_t reg, uint32_t val)
+{
+    *(volatile uint32_t *)(dev->mmio_base + reg) = val;
+}
 
 void e1000_intr_handler(void){
 
@@ -16,10 +26,33 @@ void e1000_intr_handler(void){
 
 void e1000_tx_init(struct e1000 *dev){
     //レジスタへ設定
+    uint64_t tx_reg_base = (uint64_t)(V2P(dev->tx_ring));
+    e1000_reg_write(dev, E1000_TDBAL, (uint32_t)(tx_reg_base & 0xffffffff));
+    e1000_reg_write(dev, E1000_TDBAH, (uint32_t)(tx_reg_base >> 32) );
+    // tx descriptor length
+    e1000_reg_write(dev, E1000_TDLEN, (uint32_t)(TX_RING_SIZE * sizeof(struct tx_desc)));
+    // setup head/tail
+    e1000_reg_write(dev, E1000_TDH, 0);
+    e1000_reg_write(dev, E1000_TDT, 0);
 }
 
 void e1000_rx_init(struct e1000 *dev){
     //レジスタへ設定
+    for (int n = 0; n < 128; n++){
+        e1000_reg_write(dev, E1000_MTA + (n << 2), 0);
+    }
+    uint64_t rcv_ring_base=(uint64_t)(dev->rx_ring);//V2Pした領域である必要がある？
+    e1000_reg_write(dev, E1000_RDBAL, (uint32_t)(rcv_ring_base & 0xffffffff));
+    e1000_reg_write(dev, E1000_RDBAH, (uint32_t)(rcv_ring_base >> 32));
+    // rx descriptor lengh
+    e1000_reg_write(dev, E1000_RDLEN, (uint32_t)(RX_RING_SIZE * sizeof(struct rx_desc)));
+    // setup head/tail
+    e1000_reg_write(dev, E1000_RDH, 0);
+    e1000_reg_write(dev, E1000_RDT, RX_RING_SIZE-1);
+}
+
+void e1000_open(){
+    
 }
 
 void e1000_init(void){
--- e1000.h
+++ e1000.h
@@ -4,6 +4,7 @@
 #include "types.h"
 #include "param.h"
 #include "pcireg.h"
+#include "memlayout.h"
 
 
 //defined in pci.c
